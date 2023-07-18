c4648b07354b1cb88f871f8e37b9745beaa0f481
sandeep12biswas <sandeep12.biswas@gmail.com>
2023-07-18 22:13:08 +0530

    Exception handling is modified and used

--- ExceptionHandle.py
+++ ExceptionHandle.py
@@ -1,20 +1,30 @@
-def divideNum():
-    first_num = 10
-    second_num = 2
-    try:
-        output = first_num / second_num
-        print("Result = {}".format(output))
-    except ZeroDivisionError as ex:
-        print("Exception happened because {} is divided by {} and type of exception is {}".format(first_num,
-                                                                                                  second_num,
-                                                                                                  type(ex)))
-    finally:
-        print("Use greater than zero!")
+
+class ExceptionsHandleMain:
+
+    def __init__(self):
+        self.second_num = None
+        self.first_num = None
+
+    def divideNum (self):
+
+        self.first_num = input("Please enter first number : ")
+        self.second_num = input("Please enter Second number : ")
+        o = None
+        try:
+            o = int(self.first_num) / int(self.second_num)
+        except ValueError as ve:
+            print("Please enter valid number, {} and/or {} are/is not number".format(self.first_num,self.second_num))
+
+        except ZeroDivisionError as ex:
+            print("Exception happened because {} is divided by {} and type of exception is {}".format(self.first_num,
+                                                                                                      self.second_num,
+                                                                                                      type(ex)))
+        finally:
+            print("Use greater than zero!")
+
+        return o
 
 
-class ExceptionHandle:
-    pass
-
-
-result = ExceptionHandle()
-divideNum()
+result = ExceptionsHandleMain()
+output = result.divideNum()
+print("Result = {}".format(output))
