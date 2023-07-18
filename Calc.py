58de7253e8110f14f859e0076f2f93c714b31bd1
sandeep12biswas <sandeep12.biswas@gmail.com>
2023-07-18 20:26:19 +0530

    Simple method divideNum to sample exception handling

--- Calc.py
+++ Calc.py
@@ -18,3 +18,15 @@ sum = calc.add()
 
 # print("sum = {}".format(sum))
 print("Sum of the two numbers {} and {} is {}".format(calc.first, calc.second, sum))
+
+def divideNum ():
+    first_num = 10
+    secnd_num = 3
+    try:
+        result = first_num/secnd_num;
+        print("Result = {}".format(result))
+    except ZeroDivisionError as ex :
+        print("Exception happend because {} is devided by {} and type of exception is {}".format(first_num, secnd_num,type(ex)))
+    finally:
+        print("Use gretter than zero!")
+devideNum()
